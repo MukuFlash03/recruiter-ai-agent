aaccd4c64bfb4753115b490d41a49c023a0545cf
Sanya Nijhawan <sanya301@gmail.com>
2024-09-22 09:32:26 +0000

    recruiter linking

---
+++ VERCEL_DEPLOYMENT/app/recruiter/page.tsx
@@ -0,0 +1,9 @@
+import { RecruiterDashboardComponent as RecruiterDashboardPage } from '@/components/recruiter-dashboard';
+
+export default function RecruiterDashboard() {
+  return (
+    <div >
+      <RecruiterDashboardPage />
+    </div>
+  );
+}
