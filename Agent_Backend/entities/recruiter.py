0d2d50a2099811875f5c90a5f4d925472a8537e7
Dheeraj Pai <dheerajpai@icloud.com>
2024-09-21 17:57:03 -0700

    Recruiter Models

---
+++ Agent_Backend/entities/recruiter.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+
+
+class RequiredSkill(BaseModel):
+    name: str
+    proficiency: str
+    experience: int
+    reference_context: list[str]
+
+
+class RequiredExpertise(BaseModel):
+    name: str
+    description: str
+    reference_context: list[str]
+
+
+class RequiredPersonalityTrait(BaseModel):
+    name: str
+    description: str
+    reference_context: list[str]
+
+
+class Question(BaseModel):
+    question: str
+    reference_context: list[str]
+
+
+class Questions(BaseModel):
+    questions: list[Question]
+
+
+class ExpectedAnswer(BaseModel):
+    answer: str
+    reference_context: list[str]
